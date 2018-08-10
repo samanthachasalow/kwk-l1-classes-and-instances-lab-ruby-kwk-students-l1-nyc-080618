aa6fd3f9822fde803ba3c9419ad47cd70b775eb3
samanthachasalow <samanthachasalow@chatham-nj.org>
2018-08-10 14:23:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,9 @@
 # dog.rb
 class Dog 
-  def initialize(dog)
-  @dog = dog
 end
 
-dog_one = dog_one.new("fido")
-  puts dog_one.dog
-end
-dog_two = dog_two.new("snoopy")
-  puts dog_two.dog 
-end
-dog_three = dog_three.new("lassie")
-  puts dog_three.dog 
-end 
+fido =Dog.new 
+  snoopy= Dog.new 
+    lassi= Dog.new 
   
 
